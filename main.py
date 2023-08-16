3da97a508d10a9e6fce799b1b6fa1318d06008a2
Liza <elizaveta.markushkina@yandex.ru>
2023-08-16 12:42:47 +0300

    hello

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask('my first app')
@@ -7,12 +7,15 @@ app = Flask('my first app')
 @app.route('/')
 @app.route('/index')
 def hi_flask():
-    return 'https://github.com/liza020220202'
+    return render_template('index.html')
 
 
 @app.route('/login')
 def login():
-    return 'You are on login'
+    return 'Login'
 
 
 app.run(debug=True)
+@app.route(f"/user/<username>")
+def hello(username):
+    return render_template('hello.html', username=username)
